7bea627f6876b7492495e765f830a3bb2880246e
yej <jutard@lemonde.fr>
2014-03-24 16:01:19 +0100

    switched to a modern assertion framework

--- test/index.js
+++ test/index.js
@@ -1,22 +1,26 @@
 
 var exif = require('..');
-var assert = require('better-assert');
+
+var chai = require('chai');
+var expect = chai.expect;
+chai.config.includeStack = true; // defaults to false
 
 describe('exif(file, fn)', function(){
 
   it('respond with EXIF json data', function(done){
-    exif('test/fixtures/forest.jpeg', function(err, o){
+    exif('test/fixtures/forest.jpeg', function(err, data){
       if (err) return done(err);
-      assert('forest.jpeg' === o['FileName']);
+      expect(data, 'FileName').to.have.property('FileName', 'forest.jpeg');
+      expect(data, 'width').to.have.property('ImageWidth', 900);
       done();
     });
   });
 
   it('doesn\'t wreck utf-8 data', function(done){
-    exif('test/fixtures/forest.jpeg', function(err, o){
+    exif('test/fixtures/forest.jpeg', function(err, data){
       if (err) return done(err);
-      assert(o['Caption-Abstract'] === 'Voilà l\'été !');
-      assert(o['CopyrightNotice'] === '©photo : John Smith');
+      expect(data, 'caption').to.have.property('Caption-Abstract', 'Voilà l\'été !');
+      expect(data, 'copyright').to.have.property('CopyrightNotice', '©photo : John Smith');
       done();
     });
   });
